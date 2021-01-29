f627f7dd5fdc017145b675e41dcebf3d0bb512ea
sckelrt <74903900+sckelrt@users.noreply.github.com>
2021-01-29 13:52:17 +0900

    Add files via upload

--- project-DED-Skcelet(AlexandrKudrya)/data/levels/tile_mapa.tsx
+++ project-DED-Skcelet(AlexandrKudrya)/data/levels/tile_mapa.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tile_mapa" tilewidth="60" tileheight="60" tilecount="6" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="tile_mapa" tilewidth="60" tileheight="61" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
   <image width="60" height="60" source="Безымянный.png"/>
@@ -14,9 +14,21 @@
   <image width="60" height="60" source="Безымянный3.png"/>
  </tile>
  <tile id="6">
-  <image width="60" height="60" source="песок.png"/>
+  <image width="60" height="60" source="heart_buy.png"/>
  </tile>
  <tile id="7">
-  <image width="60" height="60" source="Безымянный5.png"/>
+  <image width="59" height="61" source="chest.png"/>
+ </tile>
+ <tile id="8">
+  <image width="60" height="60" source="../../../../../project-DED-AlexandrKudrya-patch-1 (2)/project-DED-Skcelet(AlexandrKudrya)/data/levels/Безымянный5.png"/>
+ </tile>
+ <tile id="9">
+  <image width="60" height="60" source="../../../../../project-DED-AlexandrKudrya-patch-1 (2)/project-DED-Skcelet(AlexandrKudrya)/data/levels/песок.png"/>
+ </tile>
+ <tile id="10">
+  <image width="60" height="60" source="Безымянный4.png"/>
+ </tile>
+ <tile id="11">
+  <image width="60" height="60" source="Безымянный6.png"/>
  </tile>
 </tileset>
