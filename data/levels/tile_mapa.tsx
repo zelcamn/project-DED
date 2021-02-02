2d1f12f6f27a51bbf7296aefba0c11def87449e6
AlexandrKudrya <70281496+AlexandrKudrya@users.noreply.github.com>
2021-02-02 16:38:51 +0900

    Dead Screen, Смена персонажей

---
+++ data/levels/tile_mapa.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tile_mapa" tilewidth="60" tileheight="61" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="60" height="60" source="Безымянный.png"/>
+ </tile>
+ <tile id="3">
+  <image width="60" height="60" source="Безымянный1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="60" height="60" source="Безымянный2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="60" height="60" source="Безымянный3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="60" height="60" source="heart_buy.png"/>
+ </tile>
+ <tile id="7">
+  <image width="59" height="61" source="chest.png"/>
+ </tile>
+ <tile id="8">
+  <image width="60" height="60" source="Безымянный5.png"/>
+ </tile>
+ <tile id="9">
+  <image width="60" height="60" source="песок.png"/>
+ </tile>
+ <tile id="10">
+  <image width="60" height="60" source="Безымянный4.png"/>
+ </tile>
+ <tile id="11">
+  <image width="60" height="60" source="Безымянный6.png"/>
+ </tile>
+</tileset>
