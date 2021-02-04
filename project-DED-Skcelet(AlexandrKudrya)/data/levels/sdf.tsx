0a20b9a5118778a03ec138f26521ce2108d54069
sckelrt <74903900+sckelrt@users.noreply.github.com>
2021-02-04 10:17:02 +0900

    Add files via upload

---
+++ project-DED-Skcelet(AlexandrKudrya)/data/levels/sdf.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="sdf" tilewidth="60" tileheight="60" tilecount="28" columns="7">
+ <image source="../../../../stonetileset.png" width="420" height="240"/>
+</tileset>
