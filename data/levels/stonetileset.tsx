47266af9933e8d28750ce506d808c9868d605ab1
AlexandrKudrya <70281496+AlexandrKudrya@users.noreply.github.com>
2021-02-19 13:45:28 +0700

    MEGA FINAL PATCH

---
+++ data/levels/stonetileset.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="stonetileset" tilewidth="60" tileheight="60" tilecount="35" columns="7">
+ <image source="stonetileset.png" trans="ffffff" width="420" height="300"/>
+ <terraintypes>
+  <terrain name="wals" tile="8"/>
+ </terraintypes>
+ <tile id="7" terrain=",,,0"/>
+ <tile id="8" terrain=",,0,0"/>
+ <tile id="9" terrain=",,0,"/>
+ <tile id="14" terrain=",0,,0"/>
+ <tile id="15" terrain="0,0,0,0"/>
+ <tile id="16" terrain="0,,0,"/>
+ <tile id="21" terrain=",0,,"/>
+ <tile id="22" terrain="0,0,,"/>
+ <tile id="23" terrain="0,,,"/>
+</tileset>
